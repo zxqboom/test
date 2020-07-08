8f7e586c80851fd8d5a7765e45ff73221059c325
zxqboom <67859424+zxqboom@users.noreply.github.com>
2020-07-08 15:54:26 +0800

    函数化

--- ex2.py
+++ ex2.py
@@ -4,17 +4,17 @@
 from __future__ import division
 import random
 
-MAX_SCORE = 100
 TOTAL_NUM = 40
-scores = [random.randint(0,MAX_CSORE) for i in range(TOTAL_NUM)]   
-print scores
-
-# num = len(scores)
-sum_scores = sum(scores) * 1.0  # sum:容器中所有元素相加   * 1.0:转换成浮点型  或使用强制类型转换      
-average_num = sum_scores/TOTAL_NUM       
-num_under_average = len([x for x in scores if x<ave_num])  # x改成i，i常用于计数，x表示一个元素  
-print "the average scores is:%.1f" % average_num
-print "There are %d students less than average." % num_under_average
+def create_scores(total_num,max_score = 100)  # 变化多的参数放前面       =100：缺省值
+    return [random.randint(0,MAX_CSORE) for i in range(TOTAL_NUM)]   
+def filter_under_average(input_list)
+    num = len(input_list)
+    sum_scores = sum(input_list) * 1.0  # sum:容器中所有元素相加   * 1.0:转换成浮点型  或使用强制类型转换      
+    average = sum_scores/num      
+    return [x for x in input_list if x<average]  # x改成i，i常用于计数，x表示一个元素 
+  
+scores = create_scores(TOTAL_NUM) 
+temp = filterr_under_average(scores)
 
 ascending = True # 升序
 sorted_scores = sorted(scores, reverse=ascending)  
