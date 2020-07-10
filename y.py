4c1090b236b0794323280e270f4dfc992bbfbfe4
zxqboom <67859424+zxqboom@users.noreply.github.com>
2020-07-10 14:49:56 +0800

    Josephus problem

---
+++ y.py
@@ -0,0 +1,27 @@
+# 控制参数:
+
+nums = 41
+call 3
+
+# 参数定义:
+peoples = []
+for _ in range(nums):
+    peoples.append(True)
+
+result = []
+num = 1
+#主逻辑
+while(any(peoples)):
+    for index,people in enuerate(peoples):
+        if num = call:
+             peoples[index] = False
+             result.append(index+1)
+#            print(index+1)# 每轮的出局者
+#            print(peoples)# 每次的队列状态
+             num = 1
+        else:
+             num += 1
+print('-'* 25)
+print('\n总数为%d，报数为%d' % (nums，call))
+print('约瑟夫序列为：\n%s\n' % result)
+print('-'* 25)
