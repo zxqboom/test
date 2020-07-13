b72aae3268a83fe63b8701d395a9d20f83f3aeb2
zxqboom <67859424+zxqboom@users.noreply.github.com>
2020-07-13 18:59:50 +0800

    Josephus problem(面向对象

---
+++ Josephus problem.py
@@ -0,0 +1,71 @@
+def Participants(name, gender, age):
+    participant = {
+        'name' : name, 
+        'gender' : gender, 
+        'age' : age
+    }
+    return participant
+
+# 控制参数
+nums = 41
+call = 3
+
+for _ in range(nums):
+    Participants.append(True)
+
+result = []
+num = 1
+#主逻辑
+while(any(Participants)):
+    for index, people in enuerate(Participants):
+        if num = call:
+            Participants[index] = False
+            result.append(index + 1)
+#            print(index+1)#每轮的出局者
+            num = 1
+        else:
+            num += 1
+
+participant1 = Participants('Betty', 'female', 50)   
+participant2 = Participants('Charles', 'male', 48) 
+participant3 = Participants('Malclom', 'female', 35)
+participant4 = Participants('Mark', 'male', 38)
+participant5 = Participants('Joan', 'female', 44)
+participant6 = Participants('Bill', 'male', 29)
+participant7 = Participants('Niki', 'female', 36)
+participant8 = Participants('Vincent', 'male', 35)
+participant9 = Participants('Linda', 'female', 26)
+participant10 = Participants('William', 'male', 47)
+participant11 = Participants('Whitney', 'female', 34)
+participant12 = Participants('James', 'male', 39)
+participant13 = Participants('Lily', 'female', 30)
+participant14 = Participants('Henry', 'male', 27)
+participant15 = Participants('Barbara', 'female', 36)
+participant16 = Participants('Josephus', 'male', 36)
+participant17 = Participants('Elizabeth', 'female', 32)
+participant18 = Participants('Fred', 'male', 46)
+participant19 = Participants('Helen', 'female', 33)
+participant20 = Participants('Charles', 'male', 59)
+participant21 = Participants('Katharine', 'female', 45)
+participant22 = Participants('Karl', 'male', 35)
+participant23 = Participants('Fiona', 'female', 42)
+participant24 = Participants('Bob', 'male', 38)
+participant25 = Participants('Judy', 'female', 30)
+participant26 = Participants('John', 'male', 41)
+participant27 = Participants('Rudy', 'female', 36)
+participant28 = Participants('Thomas', 'male', 32)
+participant29 = Participants('Amanda', 'female', 40)
+participant30 = Participants('Paul', 'male', 36)
+participant31 = Participants('Tracy', 'female', 35)
+participant32 = Participants('Benjamin', 'male', 43)
+participant33 = Participants('Lisa', 'female', 37)
+participant34 = Participants('Dean', 'male', 46)
+participant35 = Participants('Debbie', 'female', 38)
+participant36 = Participants('Jack', 'male', 51)
+participant37 = Participants('Shelly', 'female', 31)
+participant38 = Participants('Scott', 'male', 30)
+participant39 = Participants('Mary', 'female', 33)
+participant40 = Participants('Eddy', 'male', 34)
+participant41 = Participants('Jane', 'female', 47)
+
+print(participant1)
